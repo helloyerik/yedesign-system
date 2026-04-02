ef4fb67f84c542a3d3be948e68bd1ee91446adfd
Yerik <yerik.kuanbayev@proton.me>
2026-04-02 16:39:32 +0500

    feat: add popover arrow atom

---
+++ packages/components/src/atoms/PopoverArrow/PopoverArrow.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    className?: string;
+  }>(),
+  {
+    className: "",
+  },
+);
+</script>
+
+<template>
+  <svg
+    class="mi-popover-arrow"
+    :class="className"
+    viewBox="0 0 19 9"
+    fill="none"
+    preserveAspectRatio="none"
+    aria-hidden="true"
+  >
+    <path d="M9.5 9C9.5 9 12.6667 0 19 0H0C6.33333 0 9.5 9 9.5 9Z" fill="currentColor" />
+  </svg>
+</template>
+
+<style scoped>
+.mi-popover-arrow {
+  display: block;
+  width: 100%;
+  height: 100%;
+}
+</style>
--- packages/components/src/atoms/index.ts
+++ packages/components/src/atoms/index.ts
@@ -29,5 +29,6 @@ export { default as Tag } from "./Tag";
 export { default as Tabs } from "./Tabs";
 export { default as Toast } from "./Toast";
 export { default as Tooltip } from "./Tooltip";
+export { default as PopoverArrow } from "./PopoverArrow";
 export { default as Tree } from "./Tree";
 export { default as UserAvatar } from "./UserAvatar";
